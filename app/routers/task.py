998f71ca6b602743f628488ae5ac1e5d75d5cce8
unknown <fullsucsess@gmail.com>
2024-11-01 13:20:53 +0500

    first commit

---
+++ app/routers/task.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter
+
+routerT = APIRouter(prefix="/task",tags=["task"])
+
+@routerT.get("/")
+async def all_tasks():
+    pass
+
+@routerT.get("/task_id'")
+async def task_by_id():
+    pass
+
+@routerT.post("/create")
+async def create_task():
+    pass
+
+@routerT.put("update")
+async def update_task():
+    pass
+
+@routerT.delete("/delete")
+async def delete_task():
+    pass
+
